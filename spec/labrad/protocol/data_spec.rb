75a223aa0467b39110c5de9249ba0a206c9c7279
Nelson Darkwah Oppong <n@darkwahoppong.com>
2016-05-18 19:57:10 -0600

    Refactor data specs

--- spec/labrad/protocol/data_spec.rb
+++ spec/labrad/protocol/data_spec.rb
@@ -3,196 +3,196 @@ require 'labrad/protocol/data'
 describe LabRAD::Protocol::Data do
   describe '#pack' do
     it 'raises PackError for invalid arguments' do
-      packer = LabRAD::Protocol::Data.new('s')
+      data = LabRAD::Protocol::Data.new('s')
 
-      expect { packer.pack(nil) }.to raise_error(LabRAD::PackError)
+      expect { data.pack(nil) }.to raise_error(LabRAD::PackError)
     end
 
     it 'packs boolean' do
-      packer = LabRAD::Protocol::Data.new('b')
+      data = LabRAD::Protocol::Data.new('b')
 
-      result = packer.pack(true)
+      result = data.pack(true)
       expected_result = [1].pack('c')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs signed integer' do
-      packer = LabRAD::Protocol::Data.new('i')
+      data = LabRAD::Protocol::Data.new('i')
 
-      result = packer.pack(1024)
+      result = data.pack(1024)
       expected_result = [1024].pack('l')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs unsigned integer' do
-      packer = LabRAD::Protocol::Data.new('w')
+      data = LabRAD::Protocol::Data.new('w')
 
-      result = packer.pack(1024)
+      result = data.pack(1024)
       expected_result = [1024].pack('L')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs string' do
-      packer = LabRAD::Protocol::Data.new('s')
+      data = LabRAD::Protocol::Data.new('s')
       string = 'Hello World!'
 
-      result = packer.pack(string)
+      result = data.pack(string)
       expected_result = [string.length].pack('l') + string
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs value' do
-      packer = LabRAD::Protocol::Data.new('v')
+      data = LabRAD::Protocol::Data.new('v')
       value = 1.23456789
 
-      result = packer.pack(value)
+      result = data.pack(value)
       expected_result = [value].pack('d')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs complex value' do
-      packer = LabRAD::Protocol::Data.new('c')
+      data = LabRAD::Protocol::Data.new('c')
       value = Complex(1.23, 4.56)
 
-      result = packer.pack(value)
+      result = data.pack(value)
       expected_result = [value.real, value.imag].pack('dd')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs timestamp' do
-      packer = LabRAD::Protocol::Data.new('t')
+      data = LabRAD::Protocol::Data.new('t')
       timestamp = Time.at(0) - 2_082_844_800 # 12:00am Jan 1, 1904 UTC
 
-      result = packer.pack(timestamp)
+      result = data.pack(timestamp)
       expected_result = [0, 0].pack('qq')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs cluster' do
-      packer = LabRAD::Protocol::Data.new('(ii)')
+      data = LabRAD::Protocol::Data.new('(ii)')
 
-      result = packer.pack([1, 1])
+      result = data.pack([1, 1])
       expected_result = [1, 1].pack('ll')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs array' do
-      packer = LabRAD::Protocol::Data.new('*v')
+      data = LabRAD::Protocol::Data.new('*v')
       array = [1.23, 4.56]
 
-      result = packer.pack(array)
+      result = data.pack(array)
       expected_result = [2, *array].pack('ld2')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs cluster array' do
-      packer = LabRAD::Protocol::Data.new('*(iv)')
+      data = LabRAD::Protocol::Data.new('*(iv)')
       array = [[1, 2.34], [5, 6.78]]
 
-      result = packer.pack(array)
+      result = data.pack(array)
       expected_result = [2, *array.flatten].pack('lldld')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs n-dimensional array' do
-      packer = LabRAD::Protocol::Data.new('*3i')
+      data = LabRAD::Protocol::Data.new('*3i')
       array = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
 
-      result = packer.pack(array)
+      result = data.pack(array)
       expected_result = [2, 2, 2, *array.flatten].pack('l3l8')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs none' do
-      packer = LabRAD::Protocol::Data.new('_')
+      data = LabRAD::Protocol::Data.new('_')
 
-      result = packer.pack('')
+      result = data.pack('')
       expected_result = ''
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs any with Integer' do
-      i_packer = LabRAD::Protocol::Data.new('i')
-      packer = LabRAD::Protocol::Data.new('?')
+      i_data = LabRAD::Protocol::Data.new('i')
+      data = LabRAD::Protocol::Data.new('?')
 
-      result = packer.pack(10)
-      expected_result = i_packer.pack(10)
+      result = data.pack(10)
+      expected_result = i_data.pack(10)
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs any with String' do
-      s_packer = LabRAD::Protocol::Data.new('s')
-      packer = LabRAD::Protocol::Data.new('?')
+      s_data = LabRAD::Protocol::Data.new('s')
+      data = LabRAD::Protocol::Data.new('?')
 
-      result = packer.pack('Hello World!')
-      expected_result = s_packer.pack('Hello World!')
+      result = data.pack('Hello World!')
+      expected_result = s_data.pack('Hello World!')
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs any with Float' do
-      v_packer = LabRAD::Protocol::Data.new('v')
-      packer = LabRAD::Protocol::Data.new('?')
+      v_data = LabRAD::Protocol::Data.new('v')
+      data = LabRAD::Protocol::Data.new('?')
 
-      result = packer.pack(1.23)
-      expected_result = v_packer.pack(1.23)
+      result = data.pack(1.23)
+      expected_result = v_data.pack(1.23)
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs any with Complex' do
-      c_packer = LabRAD::Protocol::Data.new('c')
-      packer = LabRAD::Protocol::Data.new('?')
+      c_data = LabRAD::Protocol::Data.new('c')
+      data = LabRAD::Protocol::Data.new('?')
 
       c = Complex(1, 1)
-      result = packer.pack(c)
-      expected_result = c_packer.pack(c)
+      result = data.pack(c)
+      expected_result = c_data.pack(c)
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs any with Time' do
-      t_packer = LabRAD::Protocol::Data.new('t')
-      packer = LabRAD::Protocol::Data.new('?')
+      t_data = LabRAD::Protocol::Data.new('t')
+      data = LabRAD::Protocol::Data.new('?')
 
       t = Time.now
-      result = packer.pack(t)
-      expected_result = t_packer.pack(t)
+      result = data.pack(t)
+      expected_result = t_data.pack(t)
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs error' do
-      packer = LabRAD::Protocol::Data.new('E')
+      data = LabRAD::Protocol::Data.new('E')
       code = 101
       message = 'Some random error'
 
-      result = packer.pack([code, message])
+      result = data.pack([code, message])
       expected_result = [code, message.length].pack('ll') + message
 
       expect(result).to eq(expected_result)
     end
 
     it 'packs error with element' do
-      packer = LabRAD::Protocol::Data.new('Ev')
+      data = LabRAD::Protocol::Data.new('Ev')
       code = 101
       message = 'Some random error'
       value = 1.23
 
-      result = packer.pack([code, message], value)
+      result = data.pack([code, message], value)
       expected_result = [code, message.length].pack('ll') +
                         message + [value].pack('d')
 
@@ -200,31 +200,31 @@ describe LabRAD::Protocol::Data do
     end
 
     it 'packs multiple elements' do
-      packer = LabRAD::Protocol::Data.new('iv')
+      data = LabRAD::Protocol::Data.new('iv')
       values = [1024, 1.34]
 
-      result = packer.pack(*values)
+      result = data.pack(*values)
       expected_result = values.pack('ld')
 
       expect(result).to eq(expected_result)
     end
 
     it 'ignores comments in curly brackets' do
-      i_packer = LabRAD::Protocol::Data.new('i')
-      packer = LabRAD::Protocol::Data.new('i{comment}')
+      i_data = LabRAD::Protocol::Data.new('i')
+      data = LabRAD::Protocol::Data.new('i{comment}')
 
-      result = packer.pack(10)
-      expected_result = i_packer.pack(10)
+      result = data.pack(10)
+      expected_result = i_data.pack(10)
 
       expect(result).to eq(expected_result)
     end
 
     it 'ignores comments after colon' do
-      i_packer = LabRAD::Protocol::Data.new('i')
-      packer = LabRAD::Protocol::Data.new('i: comment')
+      i_data = LabRAD::Protocol::Data.new('i')
+      data = LabRAD::Protocol::Data.new('i: comment')
 
-      result = packer.pack(10)
-      expected_result = i_packer.pack(10)
+      result = data.pack(10)
+      expected_result = i_data.pack(10)
 
       expect(result).to eq(expected_result)
     end
@@ -232,132 +232,132 @@ describe LabRAD::Protocol::Data do
 
   describe '#unpack' do
     it 'raises UnpackError for invalid arguments' do
-      packer = LabRAD::Protocol::Data.new('s')
+      data = LabRAD::Protocol::Data.new('s')
 
-      expect { packer.unpack('') }.to raise_error(LabRAD::UnpackError)
+      expect { data.unpack('') }.to raise_error(LabRAD::UnpackError)
     end
 
     it 'unpacks boolean' do
-      packer = LabRAD::Protocol::Data.new('b')
-      result = packer.unpack([1].pack('c'))
+      data = LabRAD::Protocol::Data.new('b')
+      result = data.unpack([1].pack('c'))
 
       expect(result).to eq([true])
     end
 
     it 'unpacks signed integer' do
-      packer = LabRAD::Protocol::Data.new('i')
-      result = packer.unpack([1024].pack('l'))
+      data = LabRAD::Protocol::Data.new('i')
+      result = data.unpack([1024].pack('l'))
 
       expect(result).to eq([1024])
     end
 
     it 'unpacks unsigned integer' do
-      packer = LabRAD::Protocol::Data.new('w')
-      result = packer.unpack([1024].pack('L'))
+      data = LabRAD::Protocol::Data.new('w')
+      result = data.unpack([1024].pack('L'))
 
       expect(result).to eq([1024])
     end
 
     it 'unpacks string' do
-      packer = LabRAD::Protocol::Data.new('s')
+      data = LabRAD::Protocol::Data.new('s')
       string = 'Hello World!'
-      result = packer.unpack([string.length].pack('l') + string)
+      result = data.unpack([string.length].pack('l') + string)
 
       expect(result).to eq([string])
     end
 
     it 'unpacks value' do
-      packer = LabRAD::Protocol::Data.new('v')
-      result = packer.unpack([1.2345].pack('d'))
+      data = LabRAD::Protocol::Data.new('v')
+      result = data.unpack([1.2345].pack('d'))
 
       expect(result).to eq([1.2345])
     end
 
     it 'unpacks complex value' do
-      packer = LabRAD::Protocol::Data.new('c')
-      result = packer.unpack([1, 1].pack('dd'))
+      data = LabRAD::Protocol::Data.new('c')
+      result = data.unpack([1, 1].pack('dd'))
 
       expect(result).to eq([Complex(1, 1)])
     end
 
     it 'unpacks timestamp' do
-      packer = LabRAD::Protocol::Data.new('t')
-      result = packer.unpack([0, 0].pack('qq'))
+      data = LabRAD::Protocol::Data.new('t')
+      result = data.unpack([0, 0].pack('qq'))
 
       expect(result).to eq([Time.at(0) - 2_082_844_800])
     end
 
     it 'unpacks cluster' do
-      packer = LabRAD::Protocol::Data.new('(ii)')
-      result = packer.unpack([1, 1].pack('ll'))
+      data = LabRAD::Protocol::Data.new('(ii)')
+      result = data.unpack([1, 1].pack('ll'))
 
       expect(result).to eq([[1, 1]])
     end
 
     it 'unpacks array' do
-      packer = LabRAD::Protocol::Data.new('*v')
+      data = LabRAD::Protocol::Data.new('*v')
       array = [1.9, 2.8, 3.7, 4.6, 5.5, 6.4, 7.3, 8.2, 9.1]
       string = [9, *array].pack('ld9')
-      result = packer.unpack(string)
+      result = data.unpack(string)
 
       expect(result).to eq([array])
     end
 
     it 'unpacks cluster array' do
-      packer = LabRAD::Protocol::Data.new('*(iv)')
+      data = LabRAD::Protocol::Data.new('*(iv)')
       array = [[1, 2.34], [5, 6.78]]
-      result = packer.unpack([2, *array.flatten].pack('lldld'))
+      result = data.unpack([2, *array.flatten].pack('lldld'))
 
       expect(result).to eq([array])
     end
 
     it 'unpacks n-dimensional array' do
-      packer = LabRAD::Protocol::Data.new('*3i')
+      data = LabRAD::Protocol::Data.new('*3i')
       array = [[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]]
       string = [3, 2, 2, *array.flatten].pack('l3l12')
-      result = packer.unpack(string)
+      result = data.unpack(string)
 
       expect(result).to eq([array])
     end
 
     it 'unpacks error' do
-      packer = LabRAD::Protocol::Data.new('E')
+      data = LabRAD::Protocol::Data.new('E')
       code = 101
       message = 'Some random error'
-      result = packer.unpack([code, message.length].pack('ll') + message)
+      result = data.unpack([code, message.length].pack('ll') + message)
 
       expect(result).to eq([[code, message]])
     end
 
     it 'unpacks error with element' do
-      packer = LabRAD::Protocol::Data.new('Ev')
+      data = LabRAD::Protocol::Data.new('Ev')
       code = 101
       message = 'Some random error'
       value = 1.23
-      result = packer.unpack([code, message.length].pack('ll') +
+      result = data.unpack([code, message.length].pack('ll') +
                              message + [value].pack('d'))
 
       expect(result).to eq([[code, message], value])
     end
 
     it 'unpacks none' do
-      packer = LabRAD::Protocol::Data.new('_')
-      result = packer.unpack('')
+      data = LabRAD::Protocol::Data.new('_')
+      result = data.unpack('')
 
       expect(result).to eq([''])
     end
 
     it 'unpacks multiple elements' do
-      packer = LabRAD::Protocol::Data.new('iv')
+      data = LabRAD::Protocol::Data.new('iv')
       values = [1024, 1.34]
-      result = packer.unpack(values.pack('ld'))
+      result = data.unpack(values.pack('ld'))
 
       expect(result).to eq(values)
     end
 
     it 'ignores any (?)' do
-      packer = LabRAD::Protocol::Data.new('i?')
-      p = proc { packer.unpack([10].pack('l')) }
+      data = LabRAD::Protocol::Data.new('i?')
+      p = proc { data.unpack([10].pack('l')) }
 
       expect(p).not_to raise_error
     end
